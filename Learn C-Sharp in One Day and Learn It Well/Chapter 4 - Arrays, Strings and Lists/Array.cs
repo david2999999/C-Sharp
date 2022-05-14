ee74e53b2002f3aeae5a5cf0575b6e8ca040dc20
David Jiang <djiang86@binghamton.edu>
2022-05-14 13:50:30 -0400

    Array basics

--- Learn C-Sharp in One Day and Learn It Well/Chapter 4 - Arrays, Strings and Lists/Array.cs
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 4 - Arrays, Strings and Lists/Array.cs
@@ -10,6 +10,31 @@ public class Array
         userAge2 = new [] {21, 22, 23, 24, 25};
         
         Console.WriteLine(userAge[0]);
+        Console.WriteLine(userAge.Length);
+        
+        int[] source = {12, 1, 5, -2, 16, 14};
+        int[] dest = {1, 2, 3, 4};
+        System.Array.Copy(source, dest, 3);
+
+        for (int i = 0; i < dest.Length; i++)
+        {
+            Console.Write(dest[i] + " ");
+        }
+        
+        Console.WriteLine();
+        
+        System.Array.Sort(source);
+        
+        for (int i = 0; i < source.Length; i++)
+        {
+            Console.Write(source[i] + " ");
+        }
+        
+        Console.WriteLine();
+
+        Console.WriteLine(System.Array.IndexOf(source, -2));
+        Console.WriteLine(System.Array.IndexOf(source, 100));
+
         Console.Read();
     }
 }
