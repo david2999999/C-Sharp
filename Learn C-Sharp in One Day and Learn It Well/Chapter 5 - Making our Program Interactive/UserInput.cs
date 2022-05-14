fb5d6d8d3e45b87d2fccf48e31114f727c2cac83
David Jiang <djiang86@binghamton.edu>
2022-05-14 18:07:43 -0400

    accepting user input

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 5 - Making our Program Interactive/UserInput.cs
@@ -0,0 +1,22 @@
+﻿namespace Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_5___Making_our_Program_Interactive;
+
+public class UserInput
+{
+    public static void Test()
+    {
+        string userName = "";
+        int userAge = 0;
+        int currentYear = 0;
+        
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine();
+        
+        Console.Write("Please enter your age: ");
+        userAge = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Please enter the current year: ");
+        currentYear = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
+    }
+}
--- Learn C-Sharp in One Day and Learn It Well/Program.cs
+++ Learn C-Sharp in One Day and Learn It Well/Program.cs
@@ -2,10 +2,12 @@
 
 using Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_2___Getting_ready_for_C_;
 using Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists;
+using Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_5___Making_our_Program_Interactive;
 using Array = Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists.Array;
 using String = Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists.String;
 
 // HelloWorld.Test();
 // Array.Test();
 // String.Test();
-Lists.Test();
+// Lists.Test();
+UserInput.Test();
