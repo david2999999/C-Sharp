75ed9fa3ff6d135f86af5983909e9dc7b16e8fa4
David Jiang <djiang86@binghamton.edu>
2022-05-14 12:35:18 -0400

    hello world

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 2 - Getting ready for C#/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_2___Getting_ready_for_C_;
+
+public class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.Read();
+    }
+}
