fae7c06eb83f1916cc220da43d2b92f12dcc97e6
David Jiang <djiang86@binghamton.edu>
2022-05-14 14:20:34 -0400

    string basics

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 4 - Arrays, Strings and Lists/String.cs
@@ -0,0 +1,30 @@
+﻿namespace Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists;
+
+public class String
+{
+    public static void Test()
+    {
+        string message = "Hello World";
+        string myName = "Hello World, " + "my name is Jamie";
+        
+        Console.WriteLine(message);
+        Console.WriteLine(myName);
+        Console.WriteLine(message.Length);
+        Console.WriteLine(message.Substring(2, 5));
+
+        string s1 = "This is Jamie";
+        string s2 = "Hello";
+        
+        Console.WriteLine(s1.Equals("This is Jamie"));
+        Console.WriteLine(s1.Equals(s2));
+
+        string[] separator = { ", ", "; " };
+        string names = "Peter, John; Andy, , David";
+        string[] substrings = names.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var substring in substrings)
+        {
+            Console.WriteLine(substring);
+        }
+    }
+}
--- Learn C-Sharp in One Day and Learn It Well/Program.cs
+++ Learn C-Sharp in One Day and Learn It Well/Program.cs
@@ -2,6 +2,7 @@
 
 using Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_2___Getting_ready_for_C_;
 using Array = Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists.Array;
+using String = Learn_C_Sharp_in_One_Day_and_Learn_It_Well.Chapter_4___Arrays__Strings_and_Lists.String;
 
 // HelloWorld.Test();
-Array.Test();
+String.Test();
