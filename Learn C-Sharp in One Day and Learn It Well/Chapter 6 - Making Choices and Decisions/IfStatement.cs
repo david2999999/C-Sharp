34c74799be6a9d5d5fd317b8e4fd0fda35626fdf
David Jiang <djiang86@binghamton.edu>
2022-05-15 11:30:44 -0400

    if - statement

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/IfStatement.cs
@@ -0,0 +1,26 @@
+﻿namespace DefaultNamespace;
+
+public class IfStatement
+{
+    public static void Test()
+    {
+        int userAge;
+        Console.Write("Please enter your age: ");
+        userAge = Convert.ToInt32(Console.ReadLine());
+        
+        if (userAge < 0 || userAge > 100)
+        {
+            Console.WriteLine("Invalid Age");
+            Console.WriteLine("Age must be between 0 and 100");
+        } 
+        else if (userAge < 18)
+            Console.WriteLine("Sorry you are underage");
+        else if (userAge < 21)
+            Console.WriteLine("You need parental consent");
+        else 
+        {
+            Console.WriteLine("Congratulations!");
+            Console.WriteLine("You may sign up for the event!");
+        }
+    }
+}
