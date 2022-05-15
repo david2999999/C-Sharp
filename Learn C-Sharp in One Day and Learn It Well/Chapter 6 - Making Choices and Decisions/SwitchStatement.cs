59977fc219e3b4562076cb3cbf8195e1c65a8fac
David Jiang <djiang86@binghamton.edu>
2022-05-15 11:45:18 -0400

    switch statement

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/SwitchStatement.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public class SwitchStatement
+{
+    public static void Test()
+    {
+        Console.Write("Enter your grade: ");
+        string userGrade = Console.ReadLine();
+
+        switch (userGrade)
+        {
+            case "A+":
+            case "A":
+                Console.WriteLine("Distinction");
+                break;
+            case "B":
+                Console.WriteLine("B Grade");
+                break;
+            case "C":
+                Console.WriteLine("C Grade");
+                break;
+            default:
+                Console.WriteLine("Fail");
+                break;
+        }
+    }
+}
