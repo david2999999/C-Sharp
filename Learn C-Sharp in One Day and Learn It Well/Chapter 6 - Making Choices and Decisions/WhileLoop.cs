af36f242d3e2cd76e232933e522ff4a292723291
David Jiang <djiang86@binghamton.edu>
2022-05-16 11:00:12 -0400

    while loop

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/WhileLoop.cs
@@ -0,0 +1,15 @@
+﻿namespace DefaultNamespace;
+
+public class WhileLoop
+{
+    public static void Test()
+    {
+        int counter = 5;
+        
+        while (counter > 0)
+        {
+            Console.WriteLine("Counter = {0}", counter);
+            counter = counter – 1;
+        }
+    }
+}
