9fa13d4c6a7a2b94f53a455eb0b079f265e1f12a
David Jiang <djiang86@binghamton.edu>
2022-05-17 09:21:27 -0400

    exception handling

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/Jump Statements/ExceptionHandling.cs
@@ -0,0 +1,28 @@
+﻿namespace DefaultNamespace;
+
+public class ExceptionHandling
+{
+    public static void Test()
+    {
+        int numerator, denominator;
+        
+        Console.Write("Please enter the numerator: ");
+        numerator = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Please enter the denominator: ");
+        denominator = Convert.ToInt32(Console.ReadLine());
+        
+        try
+        {
+            Console.WriteLine("The result is {0}.", numerator/denominator);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("---- End of Error Handling Example ----");
+        }
+    }
+}
