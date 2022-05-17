322e9768b98d99532d9ecc441cb990c50526f5bc
David Jiang <djiang86@binghamton.edu>
2022-05-17 09:07:26 -0400

    do while loop

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/DoWhileLoop.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace;
+
+public class DoWhileLoop
+{
+    public static void Test()
+    {
+        int counter = 100;
+        
+        do {
+            Console.WriteLine("Counter = {0}", counter);
+            counter++;
+        } while (counter < 0);
+    }
+}
