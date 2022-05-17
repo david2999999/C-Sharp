03d2ed024a84a1039a9f5412f063707533893b54
David Jiang <djiang86@binghamton.edu>
2022-05-17 09:10:57 -0400

    break keyword

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/Jump Statements/Break.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace;
+
+public class Break
+{
+    public static void Test()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("i = {0}", i);
+
+            if (i == 2) break;
+        }
+    }
+}
