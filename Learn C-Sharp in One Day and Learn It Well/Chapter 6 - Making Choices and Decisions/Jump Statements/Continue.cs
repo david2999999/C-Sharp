45318a84f841fe902c6e80d5e6f1bd05f17bad16
David Jiang <djiang86@binghamton.edu>
2022-05-17 09:14:51 -0400

    continue keyword

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/Jump Statements/Continue.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class Continue
+{
+    public static void Test()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("i = {0}", i);
+            
+            if (i == 2) continue;
+            
+            Console.WriteLine("I will not be printed if i=2.\n");
+        }
+    }
+}
