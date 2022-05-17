917859f18bfc5ae9bb8c714fb5691541d4ca844e
David Jiang <djiang86@binghamton.edu>
2022-05-17 09:39:22 -0400

    specific errors

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 6 - Making Choices and Decisions/Error Handling/SpecificErrors.cs
@@ -0,0 +1,26 @@
+﻿namespace DefaultNamespace;
+
+public class SpecificErrors
+{
+    public static void Test()
+    {
+        int choice = 0;
+        int[] numbers = { 10, 11, 12, 13, 14, 15 };
+        Console.Write("Please enter the index of the array: ");
+        
+        try
+        {
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("numbers[{0}] = {1}", choice, numbers[choice]);
+        } catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Error: Index should be from 0 to 5.");
+        } catch (FormatException)
+        {
+            Console.WriteLine("Error: You did not enter an integer.");
+        } catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
