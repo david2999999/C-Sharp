ef99ec007343122688f9e94a53845b2260d0a9bb
David Jiang <djiang86@binghamton.edu>
2022-06-05 11:18:36 -0400

    creating method and method overloading

--- Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Staff.cs
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Staff.cs
@@ -20,4 +20,32 @@ public class Staff
                 hWorked = 0;
         }
     }
+    
+    public int CalculatePay()
+    {
+        PrintMessage();
+        
+        int staffPay;
+        staffPay = hWorked * hourlyRate ;
+        
+        if (hWorked > 0)
+            return staffPay;
+        else
+            return 0;
+    }
+    
+    public int CalculatePay(int bonus, int allowance)
+    {
+        PrintMessage();
+        
+        if (hWorked > 0)
+            return hWorked * hourlyRate + bonus + allowance;
+        else
+            return 0;
+    }
+
+    public void PrintMessage()
+    {
+        Console.WriteLine("Calculating Pay…");
+    }
 }
