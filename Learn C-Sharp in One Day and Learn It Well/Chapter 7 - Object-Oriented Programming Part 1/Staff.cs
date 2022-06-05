947c220d278d602fb8471671d978333ff8e70624
David Jiang <djiang86@binghamton.edu>
2022-06-05 10:17:48 -0400

    getters and setters

--- Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Staff.cs
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Staff.cs
@@ -5,4 +5,19 @@ public class Staff
     private string nameOfStaff;
     private const int hourlyRate = 30;
     private int hWorked;
+    
+    public int HoursWorked
+    {
+        get
+        {
+            return hWorked;
+        }
+        set
+        {
+            if (value > 0)
+                hWorked = value;
+            else
+                hWorked = 0;
+        }
+    }
 }
