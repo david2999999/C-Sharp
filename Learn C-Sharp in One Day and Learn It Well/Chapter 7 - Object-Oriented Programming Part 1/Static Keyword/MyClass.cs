e505548da9f43c5e38d100ff1f7831495bc2ef85
David Jiang <djiang86@binghamton.edu>
2022-06-08 22:46:01 -0400

    static keyword

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Static Keyword/MyClass.cs
@@ -0,0 +1,22 @@
+﻿namespace DefaultNamespace;
+
+public class MyClass
+{
+     //Non static members
+     public string message = "Hello World";
+     public string Name { get; set; }
+     
+     public void DisplayName()
+     {
+        Console.WriteLine(“Name = {0}”, Name);
+     }
+
+     //Static members
+     public static string greetings = "Good morning";
+     public static int Age { get; set; }
+
+     public static void DisplayAge()
+     {
+         Console.WriteLine("Age = {0}", Age);
+     }
+}
