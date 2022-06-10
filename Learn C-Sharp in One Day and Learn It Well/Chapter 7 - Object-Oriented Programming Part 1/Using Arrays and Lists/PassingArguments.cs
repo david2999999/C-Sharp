9ca9887b3b819991c6b8e7fd378f66e1b880e7e4
David Jiang <djiang86@binghamton.edu>
2022-06-09 22:28:46 -0400

    passing array and list as parameters

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Using Arrays and Lists/PassingArguments.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace;
+
+public class MyClass
+{
+    public static void Test()
+    {
+        int[] myArray = {1, 2, 3, 4, 5};
+        PrintFirstElement(myArray);
+        
+        List<int> myList = new List<int> {1, 2, 3};
+        PrintFirstListElement(myList);
+    }
+    
+    public void PrintFirstElement(int[] a)
+    {
+        Console.WriteLine("The first element is {0}.\n", a[0]);
+    }
+    
+    public void PrintFirstListElement(List<int> a)
+    {
+        Console.WriteLine("The first list element is {0}.\n", a[0]);
+    }
+}
