96a21d18643eb30748680fada3e86fba955b304f
David Jiang <djiang86@binghamton.edu>
2022-06-10 22:49:49 -0400

    returning array and list from method

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Using Arrays and Lists/ReturnValues.cs
@@ -0,0 +1,34 @@
+﻿namespace DefaultNamespace;
+
+public class ReturnValues
+{
+    public int[] ReturnUserInput()
+    {
+        int[] a = new int[3];
+        
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write("Enter an integer: ");
+            a[i] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Integer added to array.\n");
+        }
+        
+        return a;
+    }
+    
+    public List<int> ReturnUserInputList()
+    {
+        List<int> a = new List<int>();
+        int input;
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Enter an integer: ");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Integer added to list.\n");
+            a.Add(input);
+        }
+        
+        return a;
+    }
+}
