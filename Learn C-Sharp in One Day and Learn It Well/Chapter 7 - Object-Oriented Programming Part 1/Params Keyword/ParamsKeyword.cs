bc5acbee3b75f6474c8d1fa88203773a70ee2f60
David Jiang <djiang86@binghamton.edu>
2022-06-10 23:03:57 -0400

    using params keyword to pass multiple arguments

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Params Keyword/ParamsKeyword.cs
@@ -0,0 +1,21 @@
+﻿namespace DefaultNamespace;
+
+public class ParamsKeyword
+{
+    public static void Test()
+    {
+        PrintNames("Peter");
+        PrintNames("Yvonne", "Jamie");
+        PrintNames("Abigail", "Betty", "Carol", "David");
+    }
+
+    public void PrintNames(params string[] names)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.Write(names[i] + " ");
+        }
+        
+        Console.WriteLine();
+    }
+}
