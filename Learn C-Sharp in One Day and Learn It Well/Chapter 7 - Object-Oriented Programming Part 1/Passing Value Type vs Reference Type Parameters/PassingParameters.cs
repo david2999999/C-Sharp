f9d51e7aabfebcedaa58839347a20c65a87940f1
David Jiang <djiang86@binghamton.edu>
2022-06-11 23:20:25 -0400

    passing value and reference types

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 7 - Object-Oriented Programming Part 1/Passing Value Type vs Reference Type Parameters/PassingParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class PassingParameters
+{
+    public void PassByValue(int a)
+    {
+        a = 10;
+        Console.WriteLine("a inside method = {0}", a);
+    }
+    
+    public void PassByReference(int[] b)
+    {
+        b[0] = 5;
+        Console.WriteLine("b[0] inside method = {0}", b[0]);
+    }
+}
