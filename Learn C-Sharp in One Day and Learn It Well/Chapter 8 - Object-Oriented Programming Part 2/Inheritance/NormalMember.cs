f4d78e1f9ce4413f51aa8cd6b571eb940fa7a1ad
David Jiang <djiang86@binghamton.edu>
2022-06-11 23:32:29 -0400

    inheritance - child class

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Inheritance/NormalMember.cs
@@ -0,0 +1,21 @@
+﻿namespace DefaultNamespace;
+
+public class NormalMember : Member
+{
+    public NormalMember(string remarks) : base ("Jamie", 1, 2015)
+    {
+        Console.WriteLine("Remarks = {0}", remarks);
+    }
+    
+    public NormalMember(string remarks, string name, int memberID, int
+        memberSince) : base (name, memberID, memberSince)
+    {
+        Console.WriteLine("Child Constructor with 4 parameters");
+        Console.WriteLine("Remarks = {0}", remarks);
+    }
+        
+    public NormalMember()
+    {
+        Console.WriteLine("Child constructor with no parameter");
+    }
+}
