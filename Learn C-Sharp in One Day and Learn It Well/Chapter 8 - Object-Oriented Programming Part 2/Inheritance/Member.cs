f58706cfebb2ab5aae025b9209a72e565b0fe6d5
David Jiang <djiang86@binghamton.edu>
2022-06-11 23:27:20 -0400

    inheritance - parent class

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Inheritance/Member.cs
@@ -0,0 +1,29 @@
+﻿namespace DefaultNamespace;
+
+public class Member
+{
+    protected int annualFee;
+    private string name;
+    private int memberID;
+    private int memberSince;
+    
+    public Member()
+    {
+        Console.WriteLine("Parent Constructor with no parameter");
+    }
+
+    public Member(string pName, int pMemberID, int pMemberSince)
+    {
+        Console.WriteLine("Parent Constructor with 3 parameters");
+        name = pName;
+        memberID = pMemberID;
+        memberSince = pMemberSince;
+    }
+
+    public override string ToString()
+    {
+        return "\nName: " + name + "\nMember ID: " + memberID +
+               "\nMember Since: " + memberSince + "\nTotal Annual Fee: " +
+               annualFee;
+    }
+}
