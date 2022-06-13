9e28009c0f75f31a2b55e5e2ad9710970852e4b3
David Jiang <djiang86@binghamton.edu>
2022-06-12 22:40:14 -0400

    another child class for inheritance

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Inheritance/VIPMember.cs
@@ -0,0 +1,15 @@
+﻿namespace DefaultNamespace;
+
+public class VIPMember : Member
+{
+    public VIPMember(string name, int memberID, int memberSince) :
+        base (name, memberID, memberSince)
+    {
+        Console.WriteLine("Child Constructor with 3 parameters");
+    }
+    
+    public void CalculateAnnualFee()
+    {
+        annualFee = 1200;
+    }
+}
