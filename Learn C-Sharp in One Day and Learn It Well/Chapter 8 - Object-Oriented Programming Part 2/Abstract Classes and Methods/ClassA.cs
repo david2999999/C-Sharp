e334612f3c65149dfc311fcee48793cc238d0383
David Jiang <djiang86@binghamton.edu>
2022-06-13 22:58:11 -0400

    abstract parent class

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Abstract Classes and Methods/ClassA.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace;
+
+public class ClassA : MyAbstractClass
+{
+    public override void PrintMessageAbstract()
+    {
+        Console.WriteLine("C# is fun!");
+    }
+}
---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Abstract Classes and Methods/MyAbstractClass.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public abstract class MyAbstractClass
+{
+    private string message = "Hello C#";
+    
+    public void PrintMessage()
+    {
+        Console.WriteLine(message);
+    }
+    
+    public abstract void PrintMessageAbstract();
+}
