bfe0b2613178bb994337e7641c761b86c846f252
David Jiang <djiang86@binghamton.edu>
2022-06-15 22:58:03 -0400

    access modifier revisited

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Access Modifiers Revisited/ClassA.cs
@@ -0,0 +1,8 @@
+﻿namespace DefaultNamespace;
+
+public class ClassA
+{
+    private int privateNum = 1;
+    public int publicNum = 2;
+    protected int protectedNum = 3;
+}
---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 8 - Object-Oriented Programming Part 2/Access Modifiers Revisited/ClassB.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class ClassB:ClassA
+{
+    public void PrintMessages()
+    {
+        //This is ok
+        Console.WriteLine(publicNum);
+        
+        //This is ok
+        Console.WriteLine(protectedNum);
+        
+        //This is NOT ok
+        Console.WriteLine(privateNum);
+    }
+}
