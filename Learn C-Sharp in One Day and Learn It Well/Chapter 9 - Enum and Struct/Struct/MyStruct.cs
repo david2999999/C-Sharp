943bbb2f1236e8c10d3a917880c29fa6e81baaf8
David Jiang <djiang86@binghamton.edu>
2022-06-22 23:21:30 -0400

    struct data type

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 9 - Enum and Struct/Struct/MyStruct.cs
@@ -0,0 +1,23 @@
+﻿struct MyStruct
+{
+    //Fields
+    private int x, y;
+    private AnotherClass myClass;
+    private Days myDays;
+
+    //Constructor
+    public MyStruct(int a, int b, int c)
+    {
+        myClass = new AnotherClass();
+        myClass.number = a;
+        x = b;
+        y = c;
+        myDays = Days.Mon;
+    }
+
+    //Method
+    public void PrintStatement()
+    {
+        Console.WriteLine("x = {0}, y = {1}, myDays = {2}", x, y, myDays);
+    }
+}
