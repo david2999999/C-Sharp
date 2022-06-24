d0b18091e43e3e43a9ed3ad78a8f29d11ef8fc52
David Jiang <djiang86@binghamton.edu>
2022-06-23 23:37:16 -0400

    reading a text file

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 11 - File Handling/Reading a Text File/ReadFile.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace;
+
+public class ReadFile
+{
+    public void readFile()
+    {
+        string path = "c:\\myFile.txt";
+        
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (sr.EndOfStream != true)
+            {
+            Console.WriteLine(sr.ReadLine());
+            }
+            
+            sr.Close();
+        }
+    }
+}
