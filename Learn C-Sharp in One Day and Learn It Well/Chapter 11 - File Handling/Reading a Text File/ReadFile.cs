f397ff74d16d0878d54a88aadfdd1ae0069c4ddf
David Jiang <djiang86@binghamton.edu>
2022-06-25 18:00:08 -0400

    read file with error handling

--- Learn C-Sharp in One Day and Learn It Well/Chapter 11 - File Handling/Reading a Text File/ReadFile.cs
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 11 - File Handling/Reading a Text File/ReadFile.cs
@@ -16,4 +16,22 @@ public class ReadFile
             sr.Close();
         }
     }
+
+	public void readFileWithErrorHandling() {
+		try
+		{
+			using (StreamReader sr = new StreamReader(path))
+			{
+				while (!sr.EndOfStream)
+				{
+					Console.WriteLine(sr.ReadLine());
+				}
+
+				sr.Close();
+			}
+		} catch (FileNotFoundException e)
+		{
+			Console.WriteLine(e.Message);
+		}
+	}
 }
