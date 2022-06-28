9c984f2ff4517021578ff7d20824a135e5f46200
David Jiang <djiang86@binghamton.edu>
2022-06-27 23:17:56 -0400

    writing to text file

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 11 - File Handling/Writing to a Text File/WriteFile.cs
@@ -0,0 +1,33 @@
+﻿namespace DefaultNamespace;
+
+public class WriteFile
+{
+    public void write()
+    {
+        //declaring the path to the file
+        string path = "myfile.txt";
+        
+        //Writing to the file
+        using(StreamWriter sw=new StreamWriter(path, true))
+        {
+            sw.WriteLine("ABC");
+            sw.WriteLine("DEF");
+            sw.Close();
+        }
+        
+        //Reading from the file
+        if (File.Exists(path))
+        {
+            using(StreamReader sr=new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+                sr.Close();
+            }
+        }
+        
+        Console.Read();
+    }
+}
