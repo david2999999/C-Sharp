7da3d629fd7f3e6aeacad912aab67ec8788a3f96
David Jiang <djiang86@binghamton.edu>
2022-06-30 23:25:26 -0400

    admin class extend staff

---
+++ Learn C-Sharp in One Day and Learn It Well/Chapter 12 - Project/Admin.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+
+public class Admin
+{
+    private const float overtimeRate = 15.5f;
+    private const float adminHourlyRate = 30f;
+    public float Overtime { get; private set; }
+    public Admin(string name) : base(name, adminHourlyRate) { }
+    
+    public override void CalculatePay()
+    {
+        base.CalculatePay();
+        if (HoursWorked > 160)
+            Overtime = overtimeRate * (HoursWorked - 160);
+    }
+    
+    public override string ToString()
+    {
+        return "\nNameOfStaff = " + NameOfStaff
+          + "\nadminHourlyRate = " + adminHourlyRate + "\nHoursWorked = " +
+          HoursWorked
+          + "\nBasicPay = " + BasicPay + "\nOvertime = " + Overtime
+          + "\n\nTotalPay = " + TotalPay;
+    }
+}
